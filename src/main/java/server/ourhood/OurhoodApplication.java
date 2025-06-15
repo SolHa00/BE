a045e3adbb925cf759ad3f44dac4e6e89a12551e
so1eeee <soljeong821@gmail.com>
2025-06-15 23:46:42 +0900

    init

---
+++ src/main/java/server/ourhood/OurhoodApplication.java
@@ -0,0 +1,13 @@
+package server.ourhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurhoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurhoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/ourhood/OurhoodApplicationTests.java
@@ -0,0 +1,13 @@
+package server.ourhood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurhoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
