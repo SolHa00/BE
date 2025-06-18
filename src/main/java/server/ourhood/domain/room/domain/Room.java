28a3bb50fb583a17cc8fe7bd2deb05c5cca924d2
so1eeee <soljeong821@gmail.com>
2025-06-19 01:36:28 +0900

    [refactor/#5] 엔티티 코드 컨벤션 적용 및 연관관계 최적화

--- src/main/java/server/ourhood/domain/room/domain/Room.java
+++ src/main/java/server/ourhood/domain/room/domain/Room.java
@@ -1,50 +1,56 @@
 package server.ourhood.domain.room.domain;
 
-import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import server.ourhood.domain.invitation.domain.Invitation;
-import server.ourhood.domain.join.domain.JoinRequest;
-import server.ourhood.domain.moment.domain.Moment;
 import server.ourhood.domain.common.BaseTimeEntity;
+import server.ourhood.domain.user.domain.User;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Getter
+@Entity(name = "room")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
+@Getter
 public class Room extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "room_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "room_id")
+	private Long id;
 
-    private Long hostId;
-    private String roomName;
-    private String roomDescription;
-    private String thumbnailImageUrl;
+	@Column(name = "room_name", nullable = false)
+	private String roomName;
 
-    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<RoomMembers> roomMembers = new ArrayList<>();
+	@Column(name = "room_description")
+	private String roomDescription;
 
-    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Moment> moments = new ArrayList<>();
+	@Column(name = "thumbnail_image_url")
+	private String thumbnailImageUrl;
 
-    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<JoinRequest> joinRequests = new ArrayList<>();
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "host_id")
+	private User host;
 
-    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Invitation> invitations = new ArrayList<>();
+	@OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<RoomMembers> roomMembers = new ArrayList<>();
 
-    @Builder
-    public Room(Long hostId, String roomName, String roomDescription, String thumbnailImageUrl) {
-        this.hostId = hostId;
-        this.roomName = roomName;
-        this.roomDescription = roomDescription;
-        this.thumbnailImageUrl = thumbnailImageUrl;
-    }
+	@Builder
+	public Room(String roomName, String roomDescription, String thumbnailImageUrl, User host) {
+		this.roomName = roomName;
+		this.roomDescription = roomDescription;
+		this.thumbnailImageUrl = thumbnailImageUrl;
+		this.host = host;
+	}
 }
