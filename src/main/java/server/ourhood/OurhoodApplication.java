27745cc25ac7e745a6dd7e193730843385aeecc3
so1eeee <soljeong821@gmail.com>
2025-06-18 18:05:39 +0900

    [chore/#3] 환경 설정 프로퍼티 자동 스캔 기능 활성화

--- src/main/java/server/ourhood/OurhoodApplication.java
+++ src/main/java/server/ourhood/OurhoodApplication.java
@@ -2,8 +2,10 @@ package server.ourhood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OurhoodApplication {
 
 	public static void main(String[] args) {
