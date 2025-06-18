5ddb67352ab8ffa2735260f992f436bd00b3224e
so1eeee <soljeong821@gmail.com>
2025-06-18 17:53:00 +0900

    [feat/#3] 사용자 및 인증 관련 도메인 모델 구현

---
+++ src/main/java/server/ourhood/domain/auth/domain/OAuthIdentifier.java
@@ -0,0 +1,29 @@
+package server.ourhood.domain.auth.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OAuthIdentifier {
+
+    @Column(name = "oauth_id")
+    private String oauthId;
+
+    @Column(name = "oauth_type")
+    @Enumerated(EnumType.STRING)
+    private OAuthType oauthType;
+
+    @Builder
+    public OAuthIdentifier(String oauthId, OAuthType oauthType) {
+        this.oauthId = oauthId;
+        this.oauthType = oauthType;
+    }
+}
---
+++ src/main/java/server/ourhood/domain/auth/domain/OAuthType.java
@@ -0,0 +1,9 @@
+package server.ourhood.domain.auth.domain;
+
+public enum OAuthType {
+    GOOGLE, KAKAO;
+
+    public static OAuthType fromName(String name) {
+        return OAuthType.valueOf(name.toUpperCase());
+    }
+}
