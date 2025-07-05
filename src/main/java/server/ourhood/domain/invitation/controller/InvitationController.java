e9f047eb7d7f2294009eaa1e5eb6a76df1ac5a12
so1eeee <solball@naver.com>
2025-07-06 02:13:41 +0900

    [refactor/#23] 수정 API HTTP Method PUT으로 변경

--- src/main/java/server/ourhood/domain/invitation/controller/InvitationController.java
+++ src/main/java/server/ourhood/domain/invitation/controller/InvitationController.java
@@ -1,9 +1,9 @@
 package server.ourhood.domain.invitation.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +29,7 @@ public class InvitationController {
 		return BaseResponse.success(response);
 	}
 
-	@PatchMapping("/api/invitations/{invitationId}")
+	@PutMapping("/api/invitations/{invitationId}")
 	public BaseResponse<Void> processInvitation(@LoginUser User user, @PathVariable Long invitationId,
 		@RequestBody ProcessInvitationRequest request) {
 		invitationService.processInvitation(user, invitationId, request);
