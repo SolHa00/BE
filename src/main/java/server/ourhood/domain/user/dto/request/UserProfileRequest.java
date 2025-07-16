a2588b4de4b2fecd59a2eb9103a9dabe04f26084
so1eeee <solball@naver.com>
2025-07-16 19:40:10 +0900

    [refactor/#33] 불필요한 UserController 및 관련 코드 삭제

--- src/main/java/server/ourhood/domain/user/dto/request/UserProfileRequest.java
+++
@@ -1,6 +0,0 @@
-package server.ourhood.domain.user.dto.request;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public record UserProfileRequest(MultipartFile profileImage) {
-}
--- src/main/java/server/ourhood/domain/user/service/UserService.java
+++ src/main/java/server/ourhood/domain/user/service/UserService.java
@@ -2,55 +2,22 @@ package server.ourhood.domain.user.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import server.ourhood.domain.user.domain.User;
 import server.ourhood.domain.user.repository.UserRepository;
 import server.ourhood.global.exception.BaseException;
 import server.ourhood.global.exception.BaseResponseStatus;
-import server.ourhood.global.s3.S3Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
 	private final UserRepository userRepository;
-	private final S3Service s3Service;
-
-	@Transactional(readOnly = true)
-	public User findUserById(Long userId) {
-		return userRepository.findById(userId)
-			.orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_USER));
-	}
 
 	@Transactional(readOnly = true)
 	public User findUserByNickname(String nickname) {
 		return userRepository.findByNickname(nickname)
 			.orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_USER));
-	}
-
-	@Transactional
-	public void createProfileImage(Long userId, MultipartFile profileImage) {
-		User user = findUserById(userId);
-		String profileImageUrl = s3Service.upload(profileImage);
-		user.updateProfileImage(profileImageUrl);
-	}
-
-	@Transactional
-	public void updateProfileImage(Long userId, MultipartFile profileImage) {
-		User user = findUserById(userId);
-		if (user.getProfileImageUrl() != null) {
-			s3Service.deleteFile(user.getProfileImageUrl());
-		}
-		String profileImageUrl = s3Service.upload(profileImage);
-		user.updateProfileImage(profileImageUrl);
-	}
-
-	@Transactional
-	public void deleteProfileImage(Long userId) {
-		User user = findUserById(userId);
-		s3Service.deleteFile(user.getProfileImageUrl());
-		user.updateProfileImage(null);
 	}
 }
