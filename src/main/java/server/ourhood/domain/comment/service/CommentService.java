af0848ae655280af35cf38d1e62ca5faec56139e
so1eeee <solball@naver.com>
2025-07-27 02:04:16 +0900

    [refactor/#45] 메서드명 및 DTO 생성 방식 변경

--- src/main/java/server/ourhood/domain/comment/service/CommentService.java
+++ src/main/java/server/ourhood/domain/comment/service/CommentService.java
@@ -26,16 +26,16 @@ public class CommentService {
 	private final MomentService momentService;
 
 	@Transactional(readOnly = true)
-	public Comment findCommentById(Long commentId) {
+	public Comment getByCommentId(Long commentId) {
 		return commentRepository.findById(commentId)
 			.orElseThrow(() -> new BaseException(NOT_FOUND_COMMENT));
 	}
 
 	@Transactional
 	public CommentCreateResponse createComment(User user, CommentCreateRequest request) {
-		Moment moment = momentService.findMomentById(request.momentId());
+		Moment moment = momentService.getByMomentId(request.momentId());
 		Comment parent = Optional.ofNullable(request.parentId())
-			.map(this::findCommentById)
+			.map(this::getByCommentId)
 			.map(p -> {
 				if (p.isReply()) {
 					throw new BaseException(INVALID_COMMENT_LEVEL);
@@ -43,26 +43,21 @@ public class CommentService {
 				return p;
 			})
 			.orElse(null);
-		Comment comment = Comment.createComment(
-			request.commentContent(),
-			moment,
-			parent,
-			user
-		);
+		Comment comment = Comment.createComment(request.commentContent(), moment, parent, user);
 		commentRepository.save(comment);
-		return CommentCreateResponse.of(comment.getId());
+		return new CommentCreateResponse(comment.getId());
 	}
 
 	@Transactional
 	public void updateComment(User user, Long commentId, CommentUpdateRequest request) {
-		Comment comment = findCommentById(commentId);
+		Comment comment = getByCommentId(commentId);
 		comment.validateOwner(user);
 		comment.updateContent(request.commentContent());
 	}
 
 	@Transactional
 	public void deleteComment(User user, Long commentId) {
-		Comment comment = findCommentById(commentId);
+		Comment comment = getByCommentId(commentId);
 		comment.validateOwner(user);
 		commentRepository.delete(comment);
 	}
--- src/main/java/server/ourhood/domain/join/dto/response/JoinRequestCreateResponse.java
+++ src/main/java/server/ourhood/domain/join/dto/response/JoinRequestCreateResponse.java
@@ -3,7 +3,4 @@ package server.ourhood.domain.join.dto.response;
 public record JoinRequestCreateResponse(
 	Long joinRequestId
 ) {
-	public static JoinRequestCreateResponse of(Long joinRequestId) {
-		return new JoinRequestCreateResponse(joinRequestId);
-	}
 }
