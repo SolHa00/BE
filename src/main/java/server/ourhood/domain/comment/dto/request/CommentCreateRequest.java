33998259ce0e2c4d84335eb20503efedf507831d
so1eeee <solball@naver.com>
2025-07-29 04:54:28 +0900

    [fix/#47] 유효성 검사 수정 및 추가

--- src/main/java/server/ourhood/domain/comment/dto/request/CommentCreateRequest.java
+++ src/main/java/server/ourhood/domain/comment/dto/request/CommentCreateRequest.java
@@ -1,10 +1,12 @@
 package server.ourhood.domain.comment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CommentCreateRequest(
-	@NotBlank(message = "모먼트 ID는 비워둘 수 없습니다.")
+	@NotNull(message = "모먼트 ID는 비워둘 수 없습니다.")
 	Long momentId,
+	@NotBlank(message = "댓글은 비워둘 수 없습니다.")
 	String commentContent,
 	Long parentId
 ) {
