be9b286f096d09aa7bc9663ca866201f8b050bf5
so1eeee <solball@naver.com>
2025-08-06 13:52:45 +0900

    [refactor/#55] DTO 생성 방식 및 유효성 검사 리팩토링

--- src/main/java/server/ourhood/domain/invitation/dto/request/InvitationCreateRequest.java
+++ src/main/java/server/ourhood/domain/invitation/dto/request/InvitationCreateRequest.java
@@ -1,9 +1,9 @@
 package server.ourhood.domain.invitation.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record InvitationCreateRequest(
-	@NotBlank(message = "방 ID는 비워둘 수 없습니다.")
+	@NotNull(message = "방 ID는 비워둘 수 없습니다.")
 	Long roomId,
 	String nickname
 ) {
--- src/main/java/server/ourhood/domain/room/dto/response/GetRoomListResponse.java
+++ src/main/java/server/ourhood/domain/room/dto/response/GetRoomListResponse.java
@@ -3,6 +3,7 @@ package server.ourhood.domain.room.dto.response;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import server.ourhood.domain.room.domain.Room;
 import server.ourhood.global.annotation.DateFormat;
 
 public record GetRoomListResponse(
@@ -19,12 +20,26 @@ public record GetRoomListResponse(
 			LocalDateTime createdAt,
 			int numOfMembers
 		) {
+			public static RoomMetadata from(Room room) {
+				return new RoomMetadata(
+					room.getId(),
+					room.getHost().getNickname(),
+					room.getCreatedAt(),
+					room.getRoomMembers().size()
+				);
+			}
 		}
 
 		public record RoomDetail(
 			String roomName,
 			String thumbnailUrl
 		) {
+			public static RoomDetail of(Room room, String thumbnailUrl) {
+				return new RoomDetail(
+					room.getName(),
+					thumbnailUrl
+				);
+			}
 		}
 	}
 }
